599decd6e337b5d434c4f789d8c9b7b0b288c7ad
Helmut Simon <helmut.simon@anu.edu.au>
2020-10-30 11:51:33 +1100

    Incorporate MVN prior.

--- python_scripts/run_bayesian_model.py
+++ python_scripts/run_bayesian_model.py
@@ -11,13 +11,14 @@ nohup python3 /Users/helmutsimon/repos/bayescoalescentest/python_scripts/run_bay
 """
 
 import numpy as np
+import pandas as pd
 import gzip, pickle
 import os, sys
 from time import time
 import click
 import scipy
 import pymc3
-from pymc3 import *
+#from pymc3 import *
 import theano
 from scitrack import CachingLogger, get_file_hexdigest
 import matplotlib
@@ -38,13 +39,11 @@ LOGGER = CachingLogger(create_dir=True)
 @click.argument('mutation_rate', type=float)
 @click.argument('length', type=float)
 @click.argument('mfilename', type=click.Path(exists=True))
-@click.argument('prior', nargs=-1, type=int)
+@click.option('-f', '--simuljobno', default=None, help='File of relative branch lengths for multivariate prior.')
 @click.option('-c', '--cv_mut', type=float, default=1e-6, help='Coefficient of variation of sequence mutation rate.')
 @click.option('-d', '--draws', type=float, default=50000)
-#@click.option('--up/--ep', default=True,
-#              help='Choose whether to use uninformative (default) or empirical prior.')
 @click.option('-dir', '--dirx', default='data', help='Directory for data and log files. Default is data')
-def main(job_no, filename, mutation_rate, length, mfilename, prior, cv_mut, draws, dirx):
+def main(job_no, filename, mutation_rate, length, mfilename, simuljobno, cv_mut, draws, dirx):
     start_time = time()
     if not os.path.exists(dirx):
         os.makedirs(dirx)
@@ -66,28 +65,17 @@ def main(job_no, filename, mutation_rate, length, mfilename, prior, cv_mut, draw
     LOGGER.log_message('Name = ' + matplotlib.__name__ + ', version = ' + matplotlib.__version__,
                        label="Imported module".ljust(30))
     draws = int(draws)
-    seq_mutation_rate = mutation_rate * length
-    sd_mutation_rate = seq_mutation_rate * cv_mut
+    seq_mut_rate = mutation_rate * length
+    sd_mut_rate = seq_mut_rate * cv_mut
     file_path = dirx + '/' + filename
     with gzip.open(file_path, 'rb') as results:
         results = pickle.load(results)
     infile = open(file_path, 'r')
     LOGGER.input_file(infile.name)
     infile.close()
-    sfs, true_branch_lengths, empirical_prior, mu, sd = results[:5]
+    sfs, true_branch_lengths = results[:2]
+    print(sfs, true_branch_lengths)
     n = len(sfs) + 1
-    if prior:
-        print('Explicit prior')
-        prior = np.array(prior)
-        assert np.all(prior > 0), 'Prior distribution parameters cannot be <=0.'
-        alpha = (mu / sd) ** 2
-        beta = alpha * seq_mutation_rate / mu
-    else:
-        print('Uninformative prior assumed')
-        prior = np.ones(n - 1)
-        alpha = 1
-        beta = 1e-10
-
     print('SFS ='.ljust(25), sfs)
     np.set_printoptions(precision=1)
     if true_branch_lengths is not None:
@@ -96,9 +84,9 @@ def main(job_no, filename, mutation_rate, length, mfilename, prior, cv_mut, draw
         print('True coalescence times = '.ljust(25), true_coal_times)
         print('True TMRCA  = '.ljust(25), '%.1f' % np.sum(true_branch_lengths))
     n_seq = np.arange(1, n)
-    thom = np.sum(sfs * n_seq) / (n * seq_mutation_rate)
+    thom = np.sum(sfs * n_seq) / (n * seq_mut_rate)
     print('Thomson est. TMRCA  = '.ljust(25), "%.1f" % thom)
-    var_thom = np.sum(sfs * n_seq * n_seq) / (2 * n * seq_mutation_rate) ** 2
+    var_thom = np.sum(sfs * n_seq * n_seq) / (2 * n * seq_mut_rate) ** 2
     print('Thomson std. error = '.ljust(25), '%.1f' % np.sqrt(var_thom), '\n')
 
     with gzip.open(mfilename, 'rb') as mx_details:
@@ -106,9 +94,39 @@ def main(job_no, filename, mutation_rate, length, mfilename, prior, cv_mut, draw
     infile = open(mfilename, 'r')
     LOGGER.input_file(infile.name)
     infile.close()
-    mcmc_model, trace = run_MCMC.run_MCMC(sfs, seq_mutation_rate, sd_mutation_rate, mx_details,
-                                     draws=draws, prior=prior, alpha=alpha, beta=beta)
-    branch_vars = run_MCMC.multiply_variates(trace)
+
+    if simuljobno:
+        print('Multivariate-normal prior')
+        variable_name = 'mvn_sample'
+        filename = dirx + '/relbrlens_' + simuljobno + '.csv'
+        rbl_variates = pd.read_csv(filename, index_col=0)
+        rbl_variates = np.array(rbl_variates)
+        infile = open(file_path, 'r')
+        LOGGER.input_file(infile.name)
+        infile.close()
+        j_n_inv = np.diag(np.arange(2, n + 1))
+        simplex_variates = j_n_inv.dot(rbl_variates.T).T
+        stick_bv = run_MCMC.StickBreaking_bv()
+        transf_variates = stick_bv.forward_val(simplex_variates)
+        mu = np.mean(transf_variates, axis=0)
+        sigma = np.cov(transf_variates, rowvar=False)
+        filename = dirx + '/brlens_' + simuljobno + '.csv'
+        branch_length_array = pd.read_csv(filename, index_col=0)
+        infile = open(filename, 'r')
+        LOGGER.input_file(infile.name)
+        infile.close()
+        branch_length_array = np.array(branch_length_array)
+        ttl_array = branch_length_array.dot(np.arange(2, n + 1).T)
+        ttl_mu = np.mean(ttl_array)
+        ttl_sigma = np.std(ttl_array)
+        mcmc_model, trace = run_MCMC.run_MCMC_mvn(sfs, seq_mut_rate, sd_mut_rate, mx_details, mu, sigma,
+                                                  ttl_mu, ttl_sigma, draws=draws)
+    else:
+        print('Uninformative Dirichlet prior assumed')
+        variable_name = 'probs'
+        mcmc_model, trace = run_MCMC.run_MCMC_Dirichlet(sfs, seq_mut_rate, sd_mut_rate, mx_details, draws=draws)
+
+    branch_vars = run_MCMC.multiply_variates(trace, variable_name)
     print('\nEst. branch lengths = '.ljust(25), np.mean(branch_vars, axis=1))
     fname = dirx + '/branch_vars_' + job_no + '.pklz'
     with gzip.open(fname, 'wb') as buff:
@@ -121,7 +139,7 @@ def main(job_no, filename, mutation_rate, length, mfilename, prior, cv_mut, draw
     try:
         matplotlib.logging.getLogger('matplotlib.font_manager').disabled = True
         fig, axs = plt.subplots(nrows=4, ncols=2, figsize=(20, 10))
-        traceplot(trace, ax=axs)
+        pymc3.traceplot(trace, ax=axs)
         plt.savefig(dirx + '/traceplots_' + job_no + '.png')
     except:
         print('Traceplot not saved.')
